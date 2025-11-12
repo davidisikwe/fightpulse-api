19280d8243c241211710d31a67a69017a13781ec
David Isikwe <disikwe@gmail.com>
2025-11-12 15:31:48 -0600

    Edited the main.ts to find deploy errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,13 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { AuthzModule } from './authz/authz.module';
-// import { UserModule } from './user/user.module';
-// import { PrismaModule } from './prisma/prisma.module';
-// import { IngestionModule } from './scrapper/ingestion.module';
-// import { ConfigModule } from '@nestjs/config';
-
-// @Module({
-
-//   imports: [ConfigModule.forRoot({ isGlobal: true }),AuthzModule, UserModule, PrismaModule, IngestionModule],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthzModule } from './authz/authz.module';
+import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { IngestionModule } from './scrapper/ingestion.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    IngestionModule,
-  ],
+  imports: [AuthzModule, UserModule, PrismaModule, IngestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
