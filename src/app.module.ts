b8eb39a84e4b8da4fc809684e8714baea5b55aaf
David Isikwe <disikwe@gmail.com>
2025-11-13 01:17:53 -0600

    Final commit for the night

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { AuthzModule } from './authz/authz.module';
+// import { UserModule } from './user/user.module';
+// import { PrismaModule } from './prisma/prisma.module';
+// import { IngestionModule } from './scrapper/ingestion.module';
+
+// @Module({
+//   imports: [AuthzModule, UserModule, PrismaModule, IngestionModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthzModule } from './authz/authz.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { IngestionModule } from './scrapper/ingestion.module';
 
 @Module({
-  imports: [AuthzModule, UserModule, PrismaModule, IngestionModule],
+  imports: [UserModule, PrismaModule, IngestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
