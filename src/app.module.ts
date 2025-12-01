a27729a2f3c4397c62c8f3ab1fe0c7a115a65f57
David Isikwe <disikwe@gmail.com>
2025-12-01 17:19:16 -0600

    Trying to fix sync user to backend route, This was done adding the authzmodule to appsmodule and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,12 +15,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthzModule } from './authz/authz.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { IngestionModule } from './scrapper/ingestion.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, IngestionModule],
+  imports: [UserModule, PrismaModule, IngestionModule, AuthzModule],
   controllers: [AppController],
   providers: [AppService],
 })
