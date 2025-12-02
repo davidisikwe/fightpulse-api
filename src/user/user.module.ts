cf7cb2a572cd21eb0317b4f2f5a2b3ad0bbceb74
David Isikwe <disikwe@gmail.com>
2025-12-02 15:15:08 -0600

    Automatic user update in database proposed fix

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { AuthzModule } from '../authz/authz.module';
 import { UserController } from './user.controller';
 
 @Module({
-  imports: [AuthzModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
